595385e39b8680cb9a7b11e8c0878c612826d916
jaesungl33 <thanh.jaesunglee@gmail.com>
2025-11-21 10:08:24 +0700

    v0.01
    With docs

---
+++ docs/AI Companion.mm
@@ -0,0 +1,78 @@
+
+<map>
+  <node ID="root" TEXT="AI Companion">
+    <node TEXT="Talk" ID="8599523ab8c5de8c403a167985c18138" STYLE="bubble" POSITION="right">
+      <node TEXT="Chat" ID="2680cf0a817f3bb7d7a0e9bc80a5bc2e" STYLE="fork"/>
+      <node TEXT="Voice" ID="8e1ab00238917cbc3a6a8211188ac3d0" STYLE="fork"/>
+      <node TEXT="Ask &amp; suggest" ID="31d4ee050ebe4872c31e64067d55eeee" STYLE="fork"/>
+      <node TEXT="Expression" ID="07b79b149130162d6b2faccd54c81e8f" STYLE="fork"/>
+      <node TEXT="Remember &amp; personalize" ID="879f9e1718afb36480f4a7a280c02182" STYLE="fork"/>
+      <node TEXT="Sing?" ID="3229f3cfb4bd3da80b703813e1e746ba" STYLE="fork"/>
+    </node>
+    <node TEXT="Assistant" ID="bb51fc2d7a57223abda6c533af0a7c32" STYLE="bubble" POSITION="right">
+      <node TEXT="LiveOps content" ID="4d612d20a297d34609cc9125b04b0e50" STYLE="fork">
+        <node TEXT="Promotion" ID="6e1c1d671202934e38fbc3e3a7451dcf" STYLE="fork"/>
+        <node TEXT="Battle Pass" ID="9c120c4d2adb2d957bd3ab96eb1464ca" STYLE="fork"/>
+        <node TEXT="Tournament" ID="4a6107d4e5ba50e518267279c8263ea0" STYLE="fork"/>
+      </node>
+      <node TEXT="Build suggestion" ID="c0c6a55a243f74cdebd35189791d6fed" STYLE="fork"/>
+      <node TEXT="Quest/ Daily quest reminder" ID="35a483d55df45083190b41a9e3d37941" STYLE="fork"/>
+      <node TEXT="Purchase suggestion" ID="143576727d0c50950a5589d3e4848732" STYLE="fork"/>
+      <node TEXT="Notification" ID="e86f746cac34a1f0e865e38f18bb5814" STYLE="fork"/>
+      <node TEXT="Gen Decal" ID="57e5e499671ae4b51b7aca48b7fc6551" STYLE="fork"/>
+    </node>
+    <node TEXT="NPC Model" ID="cafb2c776ebfbb5456d3c941ea54c56d" STYLE="bubble" POSITION="right">
+      <node TEXT="Nami (Japan)" ID="d35628d6bb2c61e4a42f1abd5cd35f6e" STYLE="fork">
+        <node TEXT="Playful / Cheerful" ID="74e94e711509c60f358537583b678433" STYLE="fork"/>
+      </node>
+      <node TEXT="Skye (American)" ID="35230ecc63a62bc81e30e81ad4700f21" STYLE="fork">
+        <node TEXT="Impulsive / Reckless" ID="5afb55e2c7fd8987ccb3c2a76037b780" STYLE="fork"/>
+      </node>
+      <node TEXT="Mei (China )" ID="c91ee46358887d18ed7163693f67f657" STYLE="fork">
+        <node TEXT="Wise / Mentor-like" ID="fc127eaa3adce8309868fd9ef385e93d" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="Knowledge Base" ID="8f0b652e7ee21a2c4ecbb8e0e4320859" STYLE="bubble" POSITION="right">
+      <node TEXT="NPC Behavior" ID="0dcf9cbf87cb6327acb0e52676b81d52" STYLE="fork">
+        <node TEXT="Personality Core" ID="0d24f066b37d64842163a84a8fcbbb9c" STYLE="fork"/>
+        <node TEXT="Tone of Voice" ID="68490e66439c8ffb765010607eb31aad" STYLE="fork"/>
+        <node TEXT="Support Function" ID="67031c6d50fc2fe584adbff824caec90" STYLE="fork"/>
+      </node>
+      <node TEXT="NPC Background" ID="17f59a5e34f2692f776694ce35485a08" STYLE="fork">
+        <node TEXT="Lore" ID="327a200263079a4dc88a323b10f75919" STYLE="fork">
+          <node TEXT="PVE/ Campaign ingame" ID="6a346fcb169b2ae02beb2f823ae58862" STYLE="fork"/>
+        </node>
+        <node TEXT="Emotional Bonding" ID="c537fe264987c5e8a09fd85fc6cf27b4" STYLE="fork">
+          <node TEXT="Reaction when win/lose" ID="7d48d0c8a93ffd74f1551908aef93e8f" STYLE="fork"/>
+        </node>
+        <node TEXT="Outfit" ID="f5d0506de9da65d6eb723df99c74b0d3" STYLE="fork"/>
+        <node TEXT="Voice effect" ID="b17466fc70b02306e8b250e07de365d0" STYLE="fork"/>
+      </node>
+      <node TEXT="Long Term memories" ID="e7362f626956b4a8e08e820e82086074" STYLE="fork">
+        <node TEXT="Chat history (summaries)" ID="e77fc5246f5f35ad4938751766f50920" STYLE="fork"/>
+        <node TEXT="Episodic (sự kiện quan trọng trong cuộc nói chuyện)" ID="beb206a1f4a2d747b55b2387dcc16151" STYLE="fork"/>
+        <node TEXT="Player Log (game history, purchase history, playing time, RR)" ID="4525fa96fd8f671bc74ccdf2ce2df77c" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="Restrict Area" ID="54ff3975f2c00e1e64b193ba16d9c815" STYLE="bubble" POSITION="right">
+      <node TEXT="NSFW Content (chat 18+, ảnh 18+, chính trị, quốc gia)" ID="3c95b4f3f00a82ec41ee9b0f1f29d57d" STYLE="fork"/>
+      <node TEXT="Gen Video, Âm thanh" ID="672d4e10025128545fe76420c4c0d956" STYLE="fork"/>
+      <node TEXT="Thông tin xuyên tạc" ID="c5402af3f43c65a2ae2d28419e16c724" STYLE="fork"/>
+      <node TEXT="Tin tức tổng hợp" ID="61189469826868e356602ea51ed6fe2e" STYLE="fork"/>
+      <node TEXT="Học bài, làm toán" ID="7af798873e7b6255309cbe20021f422c" STYLE="fork"/>
+      <node TEXT="Phân tích, v.v..." ID="b1ebbd0b716a1c37d0dfae5bf27ada49" STYLE="fork"/>
+    </node>
+    <node TEXT="Analytics" ID="be3189d0186e55695f4bbc25d48b04bc" STYLE="bubble" POSITION="right">
+      <node TEXT="Player behavior analytics" ID="6886e5cf753f60bbacc908b4042041e3" STYLE="fork"/>
+      <node TEXT="Player Interest" ID="db70a9c2f07386f1c31bf9cdbce11cda" STYLE="fork"/>
+      <node TEXT="Playstyle (tỉ lệ damage dealt vs taken, % thời gian di chuyển)" ID="fb9579385d70c3231c64d8ffb5b72061" STYLE="fork"/>
+      <node TEXT="Resource behavior (cách dùng gold/gem, upgrade path.)" ID="a72bcd9e4a56e155967ee197a4cebe1f" STYLE="fork"/>
+      <node TEXT="Session Pattern: số trận/ngày, peak time, session length" ID="31663f9c957b219d8beac86c0f78f0ff" STYLE="fork"/>
+      <node TEXT="Insight Layer" ID="f18be15394569745605304fb87023545" STYLE="fork">
+        <node TEXT="Thường chơi Skye mode Payload vào buổi tối, ưa thích DPS build" ID="a54fd03915db304c53fb70eda5b6c250" STYLE="fork"/>
+        <node TEXT="Có xu hướng rage quit khi thua 3 trận liên tiếp" ID="a7e1f7cccbfada37b743ca835904ef10" STYLE="fork"/>
+        <node TEXT="Thích thử map mới ngay ngày đầu release." ID="e06e8edd79bc62534c9805af84a126da" STYLE="fork"/>
+      </node>
+    </node>
+  </node>
+</map>
---
+++ docs/[Monetization Module] [Ta.mm
@@ -0,0 +1,150 @@
+
+<map>
+  <node ID="root" TEXT="[Monetization Module] [Tank War] Economy System">
+    <node TEXT="Các loại tiền" ID="1dc737e833c049ddb46bfe8536051807" STYLE="bubble" POSITION="right">
+      <node TEXT="Diamond" ID="5e11e30ec7b33434870e0611bb49d8e5" STYLE="fork">
+        <node TEXT="Nguồn sinh" ID="54d125551de1d689322982e1e63821bc" STYLE="fork">
+          <node TEXT="Nạp" ID="cbb2aa2fd866a19a3f8c2ef44c5be575" STYLE="fork"/>
+        </node>
+        <node TEXT="Nguồn tiêu" ID="3cd988d3d84dba2149cd47083229cb06" STYLE="fork">
+          <node TEXT="Mua Buff Premium" ID="615f6d6bd0de939f407e8c1d98197108" STYLE="fork">
+            <node TEXT="Thẻ tháng" ID="ba840bb1ed5a30d1d9f806b8beb08180" STYLE="fork"/>
+            <node TEXT="Battle Pass" ID="62f81af2f9b7c63a8c0e927a8c172360" STYLE="fork"/>
+          </node>
+          <node TEXT="Premium Content" ID="bf17e454567aa928d6a40c68be3fafb2" STYLE="fork">
+            <node TEXT="Mua Ticket Gen AI" ID="2449568a7ca1f854dc08d150e382f871" STYLE="fork"/>
+            <node TEXT="Mua Warp Premium" ID="a2452d6651a2afaac0ef487ae3e67cd0" STYLE="fork"/>
+            <node TEXT="Mua Decal Premium" ID="ced26c24a3effd8f5e45886bb046e323" STYLE="fork"/>
+            <node TEXT="Mua AI Companion" ID="4ca33a27eea0b5b1e1b918d5e8b63191" STYLE="fork"/>
+            <node TEXT="Effect Premium" ID="53e071a1f48509a52ab41e279e64f7a6" STYLE="fork"/>
+            <node TEXT="Tank Skin" ID="efea853b7e40068cda92e396b33c7611" STYLE="fork"/>
+          </node>
+        </node>
+      </node>
+      <node TEXT="Coin" ID="e81612463a9667b4c826ce20cc7d2a49" STYLE="fork">
+        <node TEXT="Nguồn sinh" ID="9d44554817a85425d64b2f6e2bc98697" STYLE="fork">
+          <node TEXT="Tính năng Ingame" ID="e6f94337a6e89027aa61f6de663a1536" STYLE="fork">
+            <node TEXT="Phần thưởng kết thúc trận đấu của các Mode" ID="776db9082937e3141ee0011e17ed693c" STYLE="fork"/>
+          </node>
+          <node TEXT="Battle Pass" ID="68eced5c12c982b2dff9271bbe8f9d6a" STYLE="fork">
+            <node TEXT="Reward Free &amp; Paid" ID="28c512da04af972b8ef663ac294fa497" STYLE="fork"/>
+          </node>
+          <node TEXT="In App Ads" ID="88fd563519001ab3ed679b40b4ff4a26" STYLE="fork"/>
+        </node>
+        <node TEXT="Nguồn tiêu" ID="20dee09384046770e524a1c004e2bbb1" STYLE="fork">
+          <node TEXT="Mua xe Tank" ID="fa9a4538d4b7895e49cf324d0cee98fd" STYLE="fork"/>
+          <node TEXT="Mua Warp" ID="af304ab8920c2f2be66ed9251fe64d67" STYLE="fork"/>
+          <node TEXT="Mua Decal" ID="54a2509c5af4293460cf47187af2fad1" STYLE="fork"/>
+          <node TEXT="Lượt Gen AI" ID="b4eda6fb00996bffc425c05d1ba11561" STYLE="fork"/>
+          <node TEXT="Mạnh hóa Artifacts" ID="744e1b18978f5c636919a768dad8fce2" STYLE="fork"/>
+        </node>
+      </node>
+    </node>
+    <node TEXT="Shop ingame" ID="291466e401469474a670fe43273d04ee" STYLE="bubble" POSITION="right">
+      <node TEXT="Bán xe Tank" ID="45ccaf27958cafecadb0a74f55f8f575" STYLE="fork">
+        <node TEXT="Bằng Coin" ID="6ec8965994f3a35ae03f08f0f4e605b4" STYLE="fork"/>
+      </node>
+      <node TEXT="Artifact" ID="3a229bbae0b6e16343b6df99dcb6946a" STYLE="fork">
+        <node TEXT="Tạo Artifact" ID="1d9e4f4e42db679039cb4600f24d2cc5" STYLE="fork"/>
+        <node TEXT="Nâng cấp Artifact" ID="9139eca966d39afbf67fa25855f69673" STYLE="fork"/>
+        <node TEXT="Combine Artifact" ID="51a23a3852015dc6791d88b270a81ed8" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="Monetization" ID="ca415d0ab4d7acfca3727338f82de9a4" STYLE="bubble" POSITION="right">
+      <node TEXT="Garage" ID="163b38f40b44a2fc6daa398f428f0c71" STYLE="fork">
+        <node TEXT="Decor xe Tank" ID="dbf8c78883430d71b1b2000d71dcbfc9" STYLE="fork">
+          <node TEXT="Wrap" ID="2e0308dda7a9f0e7b6fd2717bc4e8825" STYLE="fork">
+            <node TEXT="Phẩm chất" ID="e76982a13ae1b5b3db636cfa56cc0482" STYLE="fork">
+              <node TEXT="Normal" ID="d3f2205d5e26fbf03da7fa9fe358fd77" STYLE="fork"/>
+              <node TEXT="Premium" ID="91e7cf429a9fd51711ffe1433f2173c2" STYLE="fork"/>
+              <node TEXT="Legendary" ID="0c386e68cda2cf85545526e8683b1a97" STYLE="fork"/>
+              <node TEXT="Limited Edition" ID="873eb1ee32ca36dc0a54c9c1ca93e300" STYLE="fork"/>
+            </node>
+            <node TEXT="Tác dụng" ID="616ba92ac166556e68856317cddcbd54" STYLE="fork">
+              <node TEXT="Phủ toàn bộ Tank" ID="331de54e8bcb6c6e72d3af40a8884bc2" STYLE="fork"/>
+            </node>
+          </node>
+          <node TEXT="Decal" ID="d69b9cdaa3318a67634b5b7880fd9e3c" STYLE="fork">
+            <node TEXT="Dán các sticker/symbol trang trí lên Tank" ID="96ddec6da18adbe30179fb1b2fb4f9a3" STYLE="fork"/>
+            <node TEXT="Bộ Decal" ID="16be17dc39366cdd89164ca1f2cb6652" STYLE="fork">
+              <node TEXT="Release theo Seasonal" ID="27bfec1292fe41ecfd25830a79d02623" STYLE="fork"/>
+              <node TEXT="Release theo dịp lễ đặc biệt" ID="77a2309b45a2420f0dbe2be04be12a73" STYLE="fork"/>
+              <node TEXT="Limited Edition" ID="1e9c8e0a87c863699310e39ad54f13b1" STYLE="fork"/>
+            </node>
+            <node TEXT="Gen AI Decal" ID="dee08aa7f0a50365dcc4d49f881431fa" STYLE="fork">
+              <node TEXT="Player tự tạo Decal theo prompt AI" ID="1feb2006e3f5488dc47f643cfd09da09" STYLE="fork"/>
+            </node>
+          </node>
+          <node TEXT="UGC (Workshop)" ID="858c5f65ac85af51eb99230c2a275ce5" STYLE="fork"/>
+        </node>
+        <node TEXT="Artifact" ID="95a60ab855e725a02a489dd20dd02755" STYLE="fork">
+          <node TEXT="Đặc điểm" ID="2f2909f9d853ede8bc8963944c9ee285" STYLE="fork">
+            <node TEXT="4 slot" ID="d8cc23214338ca0274ed7e46085bd888" STYLE="fork"/>
+            <node TEXT="Nâng Phẩm chất" ID="87111769ab88499730129ad038187134" STYLE="fork">
+              <node TEXT="Normal" ID="15c208c7d5f9ce0b0794415efc4f42d7" STYLE="fork">
+                <node TEXT="Max 2 opt" ID="9c8e0cefacdbb18570c373c684f407f4" STYLE="fork"/>
+              </node>
+              <node TEXT="Rare" ID="c2eceae1ba646555642e4371bb786226" STYLE="fork">
+                <node TEXT="Max 3 opt" ID="f7567033ff8cc47e4022cce077473835" STYLE="fork"/>
+              </node>
+              <node TEXT="Epic" ID="23f00497e257420e530d9170f2cf8ba0" STYLE="fork">
+                <node TEXT="Max 4 opt" ID="1d9bf16960c3eb4bddf0c5ca9dc7aba5" STYLE="fork"/>
+              </node>
+              <node TEXT="Legendary" ID="8d7c721c1a6a8664cb030e33c610ddc0" STYLE="fork">
+                <node TEXT="Max 5 opt" ID="c22de8c9ebe6c8c81d857bd03d205b27" STYLE="fork"/>
+              </node>
+              <node TEXT="Outstanding" ID="32d969512bbb47172e1b58a4f82cbb62" STYLE="fork">
+                <node TEXT="Max 6 opt" ID="00f39be3faa60dc6b64dd9066942ffef" STYLE="fork"/>
+              </node>
+            </node>
+            <node TEXT="Theo bộ" ID="9e8ad437fdcfd86dcecde042e66646c2" STYLE="fork">
+              <node TEXT="Atk" ID="4c1ebfabcfd19e2d460a33b1ce925e9b" STYLE="fork">
+                <node TEXT="2 món/1 bộ" ID="c5b3e14f726451d53ed4581719cc6ba9" STYLE="fork"/>
+              </node>
+              <node TEXT="HP" ID="714bd40fe89c0e52247e43934ea884fc" STYLE="fork">
+                <node TEXT="2 món/1 bộ" ID="1fa75818f705d55c592cff0190d6eb44" STYLE="fork"/>
+              </node>
+              <node TEXT="Mov Speed" ID="43f065ad1157cf1540c692a15c72988e" STYLE="fork">
+                <node TEXT="4 món/1 bộ" ID="7a6baf46d783d31bc0231ca713676dde" STYLE="fork"/>
+              </node>
+              <node TEXT="Fire Rate" ID="a37261b5d41f937ac06922f4289e70db" STYLE="fork">
+                <node TEXT="4 món/1 bộ" ID="1fc8469d25557464e8d2eb4ff749500f" STYLE="fork"/>
+              </node>
+              <node TEXT="Critical Rate" ID="e8a558677aec0a03303cb7065e847480" STYLE="fork">
+                <node TEXT="2 món/1 bộ" ID="95f763129a143ca78740ef1b7d867966" STYLE="fork"/>
+              </node>
+              <node TEXT="Critical Damage" ID="475e55cd5c2a6fd7206a50c45191f06d" STYLE="fork">
+                <node TEXT="4 món/1 bộ" ID="a891fd3b6dfd3b5bdbeb1ba5541d6607" STYLE="fork"/>
+              </node>
+            </node>
+            <node TEXT="Random Option" ID="ae4f47f453c184fff76dbda1337e4452" STYLE="fork">
+              <node TEXT="Pool options" ID="8650410625913524101d341096a3677c" STYLE="fork">
+                <node TEXT="Comming soon" ID="1db99ab935673072c43cb6a049c59efe" STYLE="fork"/>
+              </node>
+            </node>
+            <node TEXT="Mở theo Achievement" ID="51366c820a5d8bdad8b404fd3d30f8d0" STYLE="fork"/>
+          </node>
+        </node>
+      </node>
+      <node TEXT="Battle Pass" ID="8ec63df846e6436885069ca3acbcba8a" STYLE="fork">
+        <node TEXT="Free" ID="5af96c8e0b27249b04883a93b0ecd225" STYLE="fork">
+          <node TEXT="Coin" ID="e5a1a7e3bfb691ee410dc0680c69abde" STYLE="fork"/>
+          <node TEXT="Decal (Gacha)" ID="c4ca51aa39db2f204c6b2009f770e0f8" STYLE="fork"/>
+          <node TEXT="Warp (Normal)" ID="13461e41a6f7d3b222362e901d2f7ca7" STYLE="fork"/>
+        </node>
+        <node TEXT="Paid" ID="02888306fad830ec1134f29bb1d69b85" STYLE="fork">
+          <node TEXT="Coin" ID="a44594c63fd067d305439461eed14c39" STYLE="fork"/>
+          <node TEXT="Ticket Gen AI" ID="c473716a42135b654de4d8b51e999c9f" STYLE="fork"/>
+          <node TEXT="Decal (Gacha)" ID="a0025a34710d2fa2c4c23df5431dfa3c" STYLE="fork"/>
+          <node TEXT="Warp (Premium)" ID="9a6a1185406f89f3d761fd875e9e7c78" STYLE="fork"/>
+        </node>
+      </node>
+      <node TEXT="In App Ads" ID="ff95b4ec97d37f6af15edc2f982fc0f7" STYLE="fork">
+        <node TEXT="Ticket Gen AI" ID="78f3182ea2f3dd4949baf5ff398841df" STYLE="fork"/>
+        <node TEXT="Coin" ID="22d348a364e10ee09368a7f4f50811a8" STYLE="fork"/>
+        <node TEXT="Nhận lượt chơi Event LiveOps" ID="d810bfc1b6e74a0833275cbfd3480229" STYLE="fork"/>
+        <node TEXT="Nhận lượt chơi Daily Dungeon (PVE)" ID="0a02d4da0cdc61113a7f1fda5f422722" STYLE="fork"/>
+      </node>
+    </node>
+  </node>
+</map>
